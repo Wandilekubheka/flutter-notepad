473954dad97da8b49eff81c2d6cede7983f96943
Wandile Kubheka <retroff01@gmail.com>
2025-07-27 13:14:16 +0200

    a single note model

---
+++ lib/data/model/note.dart
@@ -0,0 +1,10 @@
+class Note {
+  final String title;
+  final String description;
+  final int id;
+  const Note({
+    required this.title,
+    required this.description,
+    required this.id
+  });
+}
